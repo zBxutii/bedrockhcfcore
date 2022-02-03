904b0d85ee73af40fd397dcf123dae9b54d30267
zBxutii <95422142+zBxutii@users.noreply.github.com>
2022-02-03 02:18:11 -0300

    Main.php file

---
+++ src/zbqutii/hcf/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace zbqutii\hcf;
+
+use pocketmine\entity\EntityDataHelper;
+use pocketmine\entity\EntityFactory;
+
+use pocketmine\math\Vector3;
+
+use pocketmine\nbt\tag\CompoundTag;
+
+use pocketmine\player\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\utils\Config;
+
+use pocketmine\world\world
